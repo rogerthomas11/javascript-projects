d7ff714f79da79d92b399ab8d2b606721ba3ea59
Roger Thomas <rogerthomas96@outlook.com>
2024-01-18 16:56:22 -0600

    Completed exercises and examples for both the 'Strings' and 'Arrays' chapters

--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,7 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "cats";
+let name = "Sarah";
+let verb = "hop";
+let adjective = "happy";
+let color = "blue";
 
-console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun} — that allows ${name} to ${verb} with strings.`)

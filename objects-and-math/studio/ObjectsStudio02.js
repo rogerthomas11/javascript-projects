03c5377a27764ab144e1425768bedbb4e9b22626
Roger Thomas <rogerthomas96@outlook.com>
2024-02-01 20:34:02 -0600

    Worked through objects chapter studio

--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,14 +1,25 @@
 // Code your orbitCircumference function here:
-
+function orbitCircumference(radius) {
+  return Math.round(2 * Math.PI * radius);
+}
 
 // Code your missionDuration function here:
-
-
+function missionDuration(numOrbits, orbRad = 2000, speed = 28000) {
+  let time = (numOrbits * orbitCircumference(orbRad)) / speed;
+  return Math.round(time * 100)/100;
+}
 // Copy/paste your selectRandomEntry function here:
-
+function selectRandomEntry(arr) {
+  let i = Math.floor(Math.random() * arr.length);
+  return arr[i];
+}
 
 // Code your oxygenExpended function here:
-
+function oxygenExpended(astronaut, orbRad, speed) {
+  let duration = missionDuration(3, orbRad, speed);
+  let oxUsed = Math.round(astronaut.o2Used(duration) * 1000/1000);
+  return `${astronaut.name} will perform the spacewalk, which will last ${missionDuration} hours and require ${astronaut.o2Used} kg of oxygen.`;
+}
 
 // Candidate data & crew array.
 let candidateA = {

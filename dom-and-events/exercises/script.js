85124f617caa644803fc5bcdc4ba744ec0d919dc
Roger Thomas <rogerthomas96@outlook.com>
2024-02-25 18:28:07 -0600

    Completed DOM and Events exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -5,6 +5,23 @@ function init () {
 
     // Put your code for the exercises here.
     
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have lift off!';
+    });
+
+    missionAbort.addEventListener('mouseover', event => {
+        event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener('mouseout', event => {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener('click', event => {
+        if(window.confirm('Are you sure you want to abort the mission?')) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+    });
 }
 
 window.addEventListener("load", init);

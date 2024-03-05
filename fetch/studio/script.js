60d296fa3104c50a0fc19d0b798d3f868e2fae65
Roger Thomas <rogerthomas96@outlook.com>
2024-03-04 20:58:15 -0600

    worked on studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,22 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const astronauts = document.getElementById("container");
+            let i = 0;
+                astronauts.innerHTML = `
+                    <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                    </div>
+                `;
+        });
+    });
+});
